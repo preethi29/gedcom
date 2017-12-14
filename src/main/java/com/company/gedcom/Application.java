f193398ec3a3e940e8b30a326986605a1e5d1bdb
preethis <preethi.ms34@gmail.com>
2017-12-15 01:40:08 +0530

    Wire up tree creator and xml writer

--- src/main/java/com/company/gedcom/Application.java
+++ src/main/java/com/company/gedcom/Application.java
@@ -2,6 +2,7 @@ package com.company.gedcom;
 
 import com.company.gedcom.models.Node;
 import com.company.gedcom.parsers.GedcomFileParser;
+import com.company.gedcom.writers.XMLWriter;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class Application {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, URISyntaxException {
         List<Node> nodes = new GedcomFileParser().parseGedcomFile("sample.txt");
+        Node root = new TreeCreator().formTree(nodes);
+        new XMLWriter().write(root);
     }
 
 }
--- src/main/java/com/company/gedcom/writers/XMLWriter.java
+++ src/main/java/com/company/gedcom/writers/XMLWriter.java
@@ -18,7 +18,7 @@ public class XMLWriter {
     public void write(Node rootNode) throws ParserConfigurationException {
         Document dom = createEmptyDocument();
         Element rootElement = dom.createElement(rootNode.getType());
-        rootNode.getChildren().forEach(child -> createChildElement(dom, rootElement, rootNode));
+        rootNode.getChildren().forEach(child -> createChildElement(dom, rootElement, child));
         dom.appendChild(rootElement);
         try {
             Transformer tr = buildTransformer();
@@ -47,6 +47,7 @@ public class XMLWriter {
         tr.setOutputProperty(OutputKeys.METHOD, "xml");
         tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
         tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "gedcom.dtd");
         return tr;
     }
 
